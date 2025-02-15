522f6b0a60f9a855f6f9e01bc5097a02c90d9dad
OskarWiedeweg <dev.wiedeweg@gmail.com>
2025-02-15 12:56:02 +0100

    fix: auth redirect logic and enhance error handling
    Extracted auth redirect into a dedicated function for reuse and improved readability. Added handling for 401 errors by deleting auth cookies and redirecting users to the login page. This ensures better session management and user experience.

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,17 +1,27 @@
-import {type Handle, redirect} from "@sveltejs/kit";
+import {type Handle, type RequestEvent, redirect} from "@sveltejs/kit";
 import {api} from "$lib/api";
 import {handleLoadError} from "$lib/api/error";
-import {buildReturnParam} from "$lib/utils";
+import {buildReturnParam, deleteAuthCookies} from "$lib/utils";
+
+function redirectAuth(event: RequestEvent) {
+    throw redirect(302, `/auth/login?${buildReturnParam(event.url)}`);
+}
 
 export const handle: Handle = async ({event, resolve}) => {
     const accessToken = event.cookies.get("accessToken");
     if (!accessToken && !event.url.pathname.startsWith("/auth")) {
-        throw redirect(302, `/auth/login?${buildReturnParam(event.url)}`);
+        redirectAuth(event);
     } else if (accessToken) {
         event.locals.accessToken = accessToken;
         const response = await api(event).user().get();
         if (response.type === "error") {
-            throw handleLoadError(response.error);
+            return handleLoadError(response.error, (error) => {
+                if (error.status === 401) {
+                    deleteAuthCookies(event.cookies);
+                    redirectAuth(event)
+                }
+                return null;
+            });
         }
         event.locals.user = response.payload.data;
     }
